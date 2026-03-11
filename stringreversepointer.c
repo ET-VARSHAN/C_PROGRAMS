0f7b34d423f12b1759b94c8fdee93d80e240a1f4
ET-VARSHAN <158636411+ET-VARSHAN@users.noreply.github.com>
2026-03-11 09:58:00 +0530

    Add files via upload

---
+++ stringreversepointer.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char *str) {
+    int length = strlen(str);
+    char *start = str;
+    char *end = str + length - 1;
+    char temp;
+
+    // Swap characters using pointers
+    while (start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    reverseString(str);
+    printf("Reversed String: %s\n", str);
+
+    return 0;
+}
